d86a2e6fd3d816c44ea0759246d3920dceb634ba
sharanya <24wh1a0588@bvrithyderabad.edu.in>
2026-06-19 12:23:00 +0530

    my fourth commit

---
+++ server.js
@@ -0,0 +1,15 @@
+
+const express=require("express");
+const app =express();
+const port=process.env.PORT||6700;
+
+//To tell app, where static fles-html/css/js are stored
+app.use(express.static("frontend"));
+
+app.listen(port,function(){
+    console.log("App running on http://localhost:"+port);
+
+}
+);
+
+
